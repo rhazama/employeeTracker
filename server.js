45539062e4c656b8742f16a0feae0cc96badcb93
rhazama <rhazama01@gmail.com>
2022-05-09 20:58:18 -0700

    defined initialAction

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const consoletable = require('console.table');
 const util = require('util');
 const { connection } = require('.config/connection');
 
-connection.query = util.promisify{conenction.query};
+connection.query = util.promisify(conenction.query);
 
 connection.connect(function (err) {
     if(err) throw err;
@@ -12,4 +12,60 @@ connection.connect(function (err) {
 })
 
 console.table("Welcome to My Employee Tracker")
+
+const initialAction = async () => {
+    try {
+        let answer = await inquirer.prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role',
+                'Exit'
+            ]
+        });
+        switch (answer.action) {
+            case 'View Employees':
+                viewEmployees();
+                break;
+
+            case 'View Departments':
+                viewDepartments();
+                break;
+
+            case 'View Roles':
+                viewRoles();
+                break;
+
+            case 'Add Employees':
+                addEmployees();
+                break;
+
+            case 'Add Departments':
+                addDepartments();
+                break;
+
+            case 'Add Roles':
+                addRoles();
+                break;
+
+            case 'Update Employee Role':
+                employeeUpdate();
+                break;
+
+            case 'Exit':
+                connectionEnd();
+                break;
+        };
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
 
