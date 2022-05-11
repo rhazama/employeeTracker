5a9686f8544353ee086cb5e2f03594b573e83372
rhazama <rhazama01@gmail.com>
2022-05-10 17:03:43 -0700

    added structure to employee and role selection

--- server.js
+++ server.js
@@ -235,8 +235,42 @@ const employeeUpdate = async () => {
 
         let employees = await connection.query("SELECT * FROM employee");
 
-        let employeeSelection = await inquirer.prompt{[
-            
-        ]}
-    }
+        let employeeSelection = await inquirer.prompt([
+            {
+                name: 'employee',
+                type: 'list',
+                choices: employee.map((employeeName) => {
+                    return {
+                        name: employeeName.first_name + " " + employeeName.last_name,
+                        value: employeeName.id
+                    }
+                }),
+                message: 'Please choose an employee to update.'
+            }
+        ]);
+
+        let roles = await connection.query("SELECT * FROM role");
+
+        let roleSelection = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: roles.map((roleName) => {
+                    return {
+                        name: roleName.title,
+                        value: roleName.id
+                    }
+                }),
+                message: 'Please select the appropriate role for the employee.'
+            }
+        ]);
+
+        let result = await connection.query("UPDATE employee SET ? WHERE ?", [{ role_id: roleSelection.role }, {id: employeeSelection.employee }]);
+
+        console.log(`The role was successfully updated.\n`)
+        initialAction();
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
 }
